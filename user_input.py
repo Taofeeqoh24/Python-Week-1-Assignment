5aade476651d5a00427b38ffdfb1bdb526c3c776
Taofeeqoh24 <taofeeqohrabbit20@gmail.com>
2024-10-30 15:15:05 +0100

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,10 @@
+# Asking user for name
+name = input("What's your name, ")
+# Asking user for age
+age = input("How old are you? ")
+# Asking user for location
+location = input("Where do you live? ")
+
+# printing personalized language
+message = f"Hello {name}, you are {age} years old and live in {location}"
+print(message)
